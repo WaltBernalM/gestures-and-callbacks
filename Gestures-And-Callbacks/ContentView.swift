00389afbc5b9e7b5dd0bf64eb6e4f3c5c981b0a6
Walter Bernal <wltrbm@gmail.com>
2023-09-25 16:29:54 -0700

    Initial Commit

---
+++ Gestures-And-Callbacks/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gestures-And-Callbacks
+//
+//  Created by Walter Bernal Montero on 25/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gestures-And-Callbacks/Gestures_And_CallbacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gestures_And_CallbacksApp.swift
+//  Gestures-And-Callbacks
+//
+//  Created by Walter Bernal Montero on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Gestures_And_CallbacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
